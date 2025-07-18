c7c45252bb2a4a4d0a6430cd73406a2891125952
Tiziano <tizianoattivissimo@live.it>
2025-07-18 12:05:13 +0200

    Aggiungi i giorni

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 
+const giorniSettimana = [
+  'Lunedì',
+  'Martedì',
+  'Mercoledì',
+  'Giovedì',
+  'Venerdì',
+  'Sabato',
+  'Domenica',
+];
+
 function App() {
   return (
     <div className="app-container">
       <h1>Plan My Week</h1>
-      <p>Let's pianifichiamo the week!!!!</p>
+      <div className="giorni-container">
+        {giorniSettimana.map((giorno) => (
+          <div className="giorno" key={giorno}>
+            <h2>{giorno}</h2>
+            {/* Qui andranno le attività */}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
